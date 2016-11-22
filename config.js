ab4d51a529e402887ea5f16fc841abf48cc7a7c9
Michael Dudley <michael.dudley@improvingenterprises.com>
2016-11-21 18:02:18 -0600

    updating dependency versions

--- config.js
+++ config.js
@@ -12,8 +12,8 @@ System.config({
     "babel-runtime": "npm:babel-runtime@6.11.6",
     "chai": "npm:chai@3.5.0",
     "core-js": "npm:core-js@1.2.7",
-    "miruken-callback": "npm:miruken-callback@0.0.2",
-    "miruken-core": "npm:miruken-core@0.0.1",
+    "miruken-callback": "npm:miruken-callback@0.0.3",
+    "miruken-core": "npm:miruken-core@0.0.2",
     "–latest": "npm:babel-core@5.8.38",
     "–latest-runtime": "npm:babel-runtime@5.8.38",
     "github:jspm/nodelibs-assert@0.1.0": {
@@ -274,10 +274,10 @@ System.config({
       "brace-expansion": "npm:brace-expansion@1.1.6",
       "path": "github:jspm/nodelibs-path@0.1.0"
     },
-    "npm:miruken-callback@0.0.2": {
-      "miruken-core": "npm:miruken-core@0.0.1"
+    "npm:miruken-callback@0.0.3": {
+      "miruken-core": "npm:miruken-core@0.0.2"
     },
-    "npm:miruken-core@0.0.1": {
+    "npm:miruken-core@0.0.2": {
       "reflect-metadata": "npm:reflect-metadata@0.1.8"
     },
     "npm:mkdirp@0.5.1": {
